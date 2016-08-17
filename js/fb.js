53ff316f31a45f8706ddf51b6ea029cd761bb8a4
Srinivasan Rajappa <srajappa@buffalo.edu>
2016-08-17 13:22:39 -0400

    changes in fb.js

--- js/fb.js
+++ js/fb.js
@@ -1,6 +1,34 @@
 $(window).load(function(){
         $('#basicModal').modal('show');
     });
+    function statusChangeCallback(response) {
+      console.log('statusChangeCallback');
+      console.log(response);
+      // The response object is returned with a status field that lets the
+      // app know the current login status of the person.
+      // Full docs on the response object can be found in the documentation
+      // for FB.getLoginStatus().
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        //testAPI();
+      } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        document.getElementById('status').innerHTML = 'Please log ' +
+          'into this app.';
+      } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+        document.getElementById('status').innerHTML = 'Please log ' +
+          'into Facebook.';
+      }
+    }
+
+
+    function checkLoginState() {
+      FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+      });
+    }
 
     $(window).fbAsyncInit = function() {
         FB.init({
@@ -10,7 +38,9 @@ $(window).load(function(){
             cookie  : true, // enable cookies to allow the server to access the session
             xfbml   : true // parse XFBML
         });
-
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
       };
 
     function fb_login(){
@@ -36,9 +66,10 @@ $(window).load(function(){
             scope: 'publish_stream,email'
         });
     }
-    (function() {
-        var e = document.createElement('script');
-        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-        e.async = true;
-        document.getElementById('fb-root').appendChild(e);
-    }());
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
