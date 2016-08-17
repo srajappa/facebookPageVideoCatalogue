d8d694a306f29c3ebf357f2ed82895c380c7db6a
Srinivasan Rajappa <srajappa@buffalo.edu>
2016-08-17 13:25:34 -0400

    changes in fb.js1

--- js/fb.js
+++ js/fb.js
@@ -31,17 +31,32 @@ $(window).load(function(){
     }
 
     window.fbAsyncInit = function() {
-        FB.init({
-            appId   : '630325513798928',
-            oauth   : true,
-            status  : true, // check login status
-            cookie  : true, // enable cookies to allow the server to access the session
-            xfbml   : true // parse XFBML
-        });
-        FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-        });
-      };
+      FB.init({
+        appId      : '630325513798928',
+        cookie     : true,  // enable cookies to allow the server to access
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.5' // use graph api version 2.5
+      });
+
+      // Now that we've initialized the JavaScript SDK, we call
+      // FB.getLoginStatus().  This function gets the state of the
+      // person visiting this page and can return one of three states to
+      // the callback you provide.  They can be:
+      //
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into
+      //    your app or not.
+      //
+      // These three cases are handled in the callback function.
+
+      FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+      });
+
+    };
+
 
     function fb_login(){
         FB.login(function(response) {
