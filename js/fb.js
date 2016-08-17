d5ca190d44343a72feda47db828e0872bf287bda
Srinivasan Rajappa <srajappa@buffalo.edu>
2016-08-17 15:13:10 -0400

    access token:ST

--- js/fb.js
+++ js/fb.js
@@ -66,7 +66,7 @@ $(window).load(function(){
                 //console.log(response); // dump complete info
                 access_token = response.authResponse.accessToken; //get access token
                 user_id = response.authResponse.userID; //get FB UID
-                console.log(response.authResponse.accessToken);
+                //console.log(response.authResponse.accessToken);
                 $('#basicModal').modal('hide');
 
 
@@ -76,7 +76,8 @@ $(window).load(function(){
               // you can store this data into your database
                   if (response && !response.error) {
                     /* handle the result */
-                    console.log(JSON.stringify(response));
+                    //console.log(JSON.stringify(response));
+                    console.log(response.authResponse.accessToken);
                   }
                 });
 
