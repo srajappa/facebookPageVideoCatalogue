b7e0d43a907a4e47e0011da0d55d7f5b858198a8
Srinivasan Rajappa <srajappa@buffalo.edu>
2016-08-17 10:39:00 -0400

    close! try1

--- js/fb.js
+++ js/fb.js
@@ -11,7 +11,7 @@
       // Logged into your app and Facebook.
       testAPI();
       alert(response.authResponse.accessToken);
-      $modal-body.dialog("close");
+      $('#modbody').dialog("close");
       //window.location = 'https://srajappa.github.io/whence.html';
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
